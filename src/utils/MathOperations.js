afa5e1a8ea6ce7945fec267768d23afd4098563e
conrev <andrewmahisa@gmail.com>
2022-07-19 16:52:14 +1000

    Spatial hash class and Flattened Matrix oeprations

--- src/utils/MathOperations.js
+++ src/utils/MathOperations.js
@@ -59,17 +59,42 @@ export function vecSetCross(a,anr, b,bnr, c,cnr) {
     a[anr]   = b[bnr + 0] * c[cnr + 1] - b[bnr + 1] * c[cnr + 0];
 }			
 
-export function vecDiff(a, anr, b, bnr) {
-    anr *= 3; bnr *= 3;
-    let a0 = a[anr] - b[bnr], a1 = a[anr + 1] - b[bnr + 1], a2 = a[anr + 2] - b[bnr + 2];
-    return [a0, a1, a2];
+export function matGetDeterminant(A) {
+    let a11 = A[0], a12 = A[3], a13 = A[6];
+    let a21 = A[1], a22 = A[4], a23 = A[7];
+    let a31 = A[2], a32 = A[5], a33 = A[8];
+    return a11*a22*a33 + a12*a23*a31 + a13*a21*a32 - a13*a22*a31 - a12*a21*a33 - a11*a23*a32;
 }
 
-export function vecDistLine(a, anr, b, bnr, c, cnr) {
-    anr *= 3;
+export function matSetMult(A, a,anr, b,bnr) {
     bnr *= 3;
-    cnr *= 3;
-    
-    vecSetDiff(BminA, 0, b, bnr, a, anr, 1.0);
-    vecSetDiff(CminA, 0, c, cnr, a, anr, 1.0);      
+    var bx = b[bnr++];
+    var by = b[bnr++];
+    var bz = b[bnr];
+    vecSetZero(a,anr);
+    vecAdd(a,anr, A,0, bx);
+    vecAdd(a,anr, A,1, by);
+    vecAdd(a,anr, A,2, bz);
+}
+
+export function matSetInverse(A) {
+    let det = matGetDeterminant(A);
+    if (det == 0.0) {
+        for (let i = 0; i < 9; i++)
+            A[anr + i] = 0.0;
+            return;
+    }
+    let invDet = 1.0 / det;
+    let a11 = A[0], a12 = A[3], a13 = A[6];
+    let a21 = A[1], a22 = A[4], a23 = A[7];
+    let a31 = A[2], a32 = A[5], a33 = A[8]
+    A[0] =  (a22 * a33 - a23 * a32) * invDet; 
+    A[3] = -(a12 * a33 - a13 * a32) * invDet;
+    A[6] =  (a12 * a23 - a13 * a22) * invDet;
+    A[1] = -(a21 * a33 - a23 * a31) * invDet;
+    A[4] =  (a11 * a33 - a13 * a31) * invDet;
+    A[7] = -(a11 * a23 - a13 * a21) * invDet;
+    A[2] =  (a21 * a32 - a22 * a31) * invDet;
+    A[5] = -(a11 * a32 - a12 * a31) * invDet;
+    A[8] =  (a11 * a22 - a12 * a21) * invDet;
 }
---
+++ src/utils/SpatialHash.js
@@ -0,0 +1,96 @@
+/**
+ * Implementation of 2D grid spatial hashing
+ * 
+ * Original implementation by Matthias Muller Fischer
+ * https://github.com/matthias-research/pages/blob/master/tenMinutePhysics/12-softBodySkinning.html
+ */
+
+class SpatialHash {
+    constructor(spacing, maxNumObjects) 
+    {
+        this.spacing = spacing;
+        this.tableSize = 2 * maxNumObjects;
+        this.cellStart = new Int32Array(this.tableSize + 1);
+        this.cellEntries = new Int32Array(maxNumObjects);
+        this.queryIds = new Int32Array(maxNumObjects);
+        this.querySize = 0;
+    }
+
+    hashCoords(xi, yi, zi) {
+        /**
+         * The actual hash function
+         * for the love of god please dont use this in production.
+         */
+        var h = (xi * 92837111) ^ (yi * 689287499) ^ (zi * 283923481);	
+        return Math.abs(h) % this.tableSize; 
+    }
+
+    intCoord(coord) {
+        return Math.floor(coord / this.spacing);
+    }
+
+    hashPos(pos, nr) {
+        return this.hashCoords(
+            this.intCoord(pos[3 * nr]), 
+            this.intCoord(pos[3 * nr + 1]),
+            this.intCoord(pos[3 * nr + 2]));
+    }
+
+    create(pos) {
+        var numObjects = Math.min(pos.length / 3, this.cellEntries.length);
+
+        // determine cell sizes
+
+        this.cellStart.fill(0);
+        this.cellEntries.fill(0);
+
+        for (var i = 0; i < numObjects; i++) {
+            var h = this.hashPos(pos, i);
+            this.cellStart[h]++;
+        }
+
+        // determine cells starts
+
+        var start = 0;
+        for (var i = 0; i < this.tableSize; i++) {
+            start += this.cellStart[i];
+            this.cellStart[i] = start;
+        }
+        this.cellStart[this.tableSize] = start;	// guard
+
+        // fill in objects ids
+
+        for (var i = 0; i < numObjects; i++) {
+            var h = this.hashPos(pos, i);
+            this.cellStart[h]--;
+            this.cellEntries[this.cellStart[h]] = i;
+        }
+    }
+
+    query(pos, nr, maxDist) {
+        var x0 = this.intCoord(pos[3 * nr] - maxDist);
+        var y0 = this.intCoord(pos[3 * nr + 1] - maxDist);
+        var z0 = this.intCoord(pos[3 * nr + 2] - maxDist);
+
+        var x1 = this.intCoord(pos[3 * nr] + maxDist);
+        var y1 = this.intCoord(pos[3 * nr + 1] + maxDist);
+        var z1 = this.intCoord(pos[3 * nr + 2] + maxDist);
+
+        this.querySize = 0;
+
+        for (var xi = x0; xi <= x1; xi++) {
+            for (var yi = y0; yi <= y1; yi++) {
+                for (var zi = z0; zi <= z1; zi++) {
+                    var h = this.hashCoords(xi, yi, zi);
+                    var start = this.cellStart[h];
+                    var end = this.cellStart[h + 1];
+
+                    for (var i = start; i < end; i++) {
+                        this.queryIds[this.querySize] = this.cellEntries[i];
+                        this.querySize++;
+                    }
+                }
+            }
+        }
+    }
+};
